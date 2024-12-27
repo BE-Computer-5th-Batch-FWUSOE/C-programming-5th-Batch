7eca98680739b21dc33a8dd0562e1441aee7a969
Prabin Bhatt <bhattsamir2062@gmail.com>
2024-12-27 19:40:23 +0545

    Prabin: Program to find sum of two number

--- fourth.c
+++ fourth.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
+<<<<<<< HEAD
 int main() {
     printf("this is our first resprository in collaboration ");
     return 0;
 
 }
+=======
+int main()
+{
+    int a,b,sum;
+    printf("Enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("The sum of a and b is %d\n",sum);
+    return 0;
+}
+>>>>>>> 9cd4ed4 (Prabin: Program to find sum of two number)
