64dee79abe8fdc56eff82162b32da15b84abbeb6
itscybervivek <er.vivek.social@gmail.com>
2024-12-27 19:49:47 +0545

    My code

---
+++ Vivek/Chapter 8 practise set/03_problem3.c
@@ -0,0 +1,20 @@
+/*Write your own version if strlen function from <string.h>*/
+#include<stdio.h>
+int strlen(char str[]){
+    int i = 0, count;
+    char c = str[i];
+    
+    while (c!='\0')
+    {
+        c = str[i];
+        i++;
+    }
+    count = i - 1;
+    return count;
+}
+int main(){
+    char str[] = "Vivek bhai";
+    strlen(str);
+    printf("%d", strlen(str));
+    return 0;
+}
---
+++ Vivek/Chapter 8 practise set/04_problem4.c
@@ -0,0 +1,26 @@
+/*Write a function slice() to slice a string. It should change the original string such that it is now the sliced string. Take m and n as the start and ending position for slice.*/
+#include<stdio.h>
+
+int main(){
+    char str[] = "Dipesh";
+    int i = 0, count;
+    char c = str[i];
+    while (c!='\0')
+    {
+        c = str[i];
+        i++;
+    }
+    count = i - 1;
+    printf("String contains %d character.", count);
+    if (count%2 == 0)
+    {
+        int chr = 0;
+        while (char ==)
+        {
+            /* code */
+        }
+        
+    }
+    
+    return 0;
+}
