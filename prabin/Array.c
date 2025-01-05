acac654479b10f7c83b1d02adbdf6bff27642028
Prabin Bhatt <bhattsamir2062@gmail.com>
2025-01-05 20:04:26 +0545

    Prabin:Program to arrange an elements of array in ascending order

---
+++ prabin/Array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void sortArray(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    sortArray(arr, n);
+
+    printf("Array in ascending order:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
